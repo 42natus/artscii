75406f021b766e9e4aafdce36a7c7099c2e3c0c0
foloruns <fortunatus.olorunsola@gmail.com>
2026-05-19 15:40:15 +0100

    wip: `Draw()` returns `[][]Word`
    To account for justifying user input, `Draw()` has been redesigned to
    return `[][]Word`. i.e. one `[]Word` per input line, one `Word` per
    space-separated word.

--- art/draw.go
+++ art/draw.go
@@ -4,26 +4,37 @@ import (
 	"strings"
 )
 
-func Draw(input, banner string) []Word {
+func Draw(input, banner string) [][]Word {
 	template := GenerateTemplate(banner)
 	if template == nil {
 		return nil
 	}
-
-	var words []Word
-	for line := range strings.SplitSeq(input, "\\n") {
-		words = append(words, render(line, template))
+	
+	var words [][]Word
+	
+	for field := range strings.SplitSeq(input, "\\n") {
+		if strings.Contains(field, " ") { // generate each whitespace-separated word
+			var line []Word
+			for subWord := range strings.SplitSeq(field, " ") {
+				line = append(line, render(subWord, template))
+				// line = append(line, render(" ", template))
+			}
+		} else {
+			var line []Word
+			line = append(line, render(field, template))
+			words = append(words, line)
+		}
 	}
 
 	return words
 }
 
 // generates the ASCII art for each line its called on
-func render(line string, template []string) Word {
-	r := []rune(line)
+func render(s string, template []string) Word {
+	r := []rune(s)
 	word := make(Word, len(r))
 
-	for i, ch := range line {
+	for i, ch := range s {
 		start := (ch-' ')*9 + 1
 		word[i] = template[start : start+8]
 	}

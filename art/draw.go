c53f74c417f9a7e24fb2b7db42301c5ea19e0473
foloruns <fortunatus.olorunsola@gmail.com>
2026-05-19 14:47:38 +0100

    refactor: split rendering logic from "\n" handling
    To simplify `Draw()`, the actual ASCII art generating logic was placed
    in a renderer function (`render()`). `Draw()` simply splits the user
    input into fields and passes each field to the renderer.

--- art/draw.go
+++ art/draw.go
@@ -12,15 +12,20 @@ func Draw(input, banner string) []Word {
 
 	var words []Word
 	for line := range strings.SplitSeq(input, "\\n") {
-		r := []rune(line)
-		word := make(Word, len(r))
-
-		for i, ch := range line {
-			start := (ch-' ')*9 + 1
-			word[i] = template[start : start+8]
-		}
-		words = append(words, word)
+		words = append(words, render(line, template))
 	}
 
 	return words
+}
+
+// generates the ASCII art for each line its called on
+func render(line string, template []string) Word {
+	r := []rune(line)
+	word := make(Word, len(r))
+
+	for i, ch := range line {
+		start := (ch-' ')*9 + 1
+		word[i] = template[start : start+8]
+	}
+	return word
 }
